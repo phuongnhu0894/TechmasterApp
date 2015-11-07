c8c46a56e4649c30b7a9f4da4cc83501e56c17ef
Phuong <phuongnhu0894@gmail.com>
2015-11-07 14:11:13 +0700

    Second Commit

--- TechmasterApp/DemoArray.m
+++ TechmasterApp/DemoArray.m
@@ -17,13 +17,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSArray *array1 = [NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"data" ofType:@"plist"]];
-    for (NSDictionary* item in array1) {
-        NSLog(@"%@.%@ (%@)", item[@"Number"], item[@"Name"], item[@"Position"]);
-    }
+//    NSArray *array1 = [NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"data" ofType:@"plist"]];
+//    for (NSDictionary* item in array1) {
+//        NSLog(@"%@.%@ (%@)", item[@"Number"], item[@"Name"], item[@"Position"]);
+//    }
     
-    
-    
+    NSBundle* bundle = [NSBundle mainBundle];
+    NSString* plistPath = [bundle pathForResource:@"data" ofType:@"plist"];
+    NSDictionary* plisttext2 = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+    NSArray* keys = [plisttext2 allKeys];
+    int randomIndex = arc4random() % (keys.count);
+    NSString* key = [plisttext2 objectForKey:[keys objectAtIndex:randomIndex]];
 
 }
 
